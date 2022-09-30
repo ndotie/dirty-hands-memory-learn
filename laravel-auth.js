cf717c987f320306a38d3575fc13244b743eaa5a
ndotie <66471341+ndotie@users.noreply.github.com>
2022-09-30 13:20:36 +0300

    Update laravel-auth.js

--- laravel-auth.js
+++ laravel-auth.js
@@ -1,1 +1,4 @@
 https://stackoverflow.com/questions/73035709/laravel-9-how-to-prevent-showing-login-page-after-user-is-logged-in-and-hit-br
+
+session-logout
+https://codeanddeploy.com/blog/laravel/laravel-8-logout-for-your-authenticated-user
