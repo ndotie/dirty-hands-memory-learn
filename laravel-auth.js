70d2ef850f22d8641df89e18e8438d3b91b2cc8c
ndotie <66471341+ndotie@users.noreply.github.com>
2022-10-01 17:09:25 +0300

    Update laravel-auth.js

--- laravel-auth.js
+++ laravel-auth.js
@@ -2,3 +2,6 @@ https://stackoverflow.com/questions/73035709/laravel-9-how-to-prevent-showing-lo
 
 session-logout
 https://codeanddeploy.com/blog/laravel/laravel-8-logout-for-your-authenticated-user
+
+IMAGE UPLOAD
+https://stackoverflow.com/questions/40358510/resize-image-in-laravel-5-2
